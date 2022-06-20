1fc69bb47b4cf6d8ad5dee8717ee7837b760e1fd
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-06-20 19:38:06 +0700

    Do excersises about objects

---
+++ Excersises/object-excersises-javascript/app.js
@@ -0,0 +1,77 @@
+// Viết 1 chương trình kiểm tra value đầu vào có phải là object hay không
+// typeof value === "object" -> Không dùng được vì nếu truyền vào array thì array cũng được coi là một object
+// {} [] null -> object array null đều đc tính là object
+
+function isObject(value) {
+  if (typeof value === "object" && !Array.isArray(value) && value !== null) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const user = {
+  name: "toilatofu",
+  age: 19,
+};
+
+isObject(user);
+
+// Bài 2: {a: 1, b: 2} -> [["a",1],["b",2]]
+
+const numbers = {
+  a: 1,
+  b: 2,
+};
+
+const objectToArray = (obj) => {
+  // check nếu không phải object thì dừng
+  if (!isObject(obj)) return;
+  // Nếu là object thì xử lý
+  // cách 1: sử dụng Object.entries
+  // if (isObject(obj)) return Object.entries(obj);
+  // cách 2: sử dung object.keys và map
+  // const value = Object.keys(obj).map((key) => [key, obj[key]]);
+  // return value;
+  // cách 3: sử dụng for in loop
+  // let result = [];
+  // for (let props in obj) {
+  //   result.push([props, obj[props]]);
+  // }
+  // return result;
+  // cách 4: sử dụng for in loop và hasOwnProperty(key)
+  // hasOwnProperty(keys) -> nếu object chứa key thì trả về true ngược lại trả về false
+  let result = [];
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push([key, obj[key]]);
+    }
+  }
+  return result;
+};
+
+console.log(objectToArray({ a: 1, b: 2 }));
+
+// Bài 3: Truyền vào bên trong hàm 1 object và sau đó là 1 key bạn muốn xóa
+// VD: ({a: 1, b: 2} , 'b') -> {a:1}
+
+const deleteAKey = (obj, ...key) => {
+  const newObj = Object.assign({}, obj); // Clone ra thành một obj mới
+  // hoặc dùng spread operator
+  // const newObj2 = { ...obj };
+  delete newObj[key];
+  return newObj;
+};
+
+console.log(deleteAKey({ a: 1, b: 2, c: 2 }, "a", "b"));
+
+// Trường hợp bạn muốn xóa nhiều key trong một object thì sao
+
+const deleteKeys = (obj, ...keys) => {
+  keys.forEach((item) => {
+    delete obj[item];
+  });
+  return obj;
+};
+
+console.log(deleteKeys({ a: 1, b: 2, c: 3 }, "a", "b"));
---
+++ destructuring-object-javascript/app.js
@@ -0,0 +1,43 @@
+// Destructuring Object
+const student = {
+  name: "Khoi",
+  age: 19,
+  isMale: true,
+  isDeveloper: true,
+};
+
+const { name, age, isMale, ...rest } = student;
+console.log(name, age, isMale, rest);
+
+function whatYourInfo(name, age, isMale) {
+  console.log(name, age, isMale);
+}
+
+whatYourInfo("Tofu", 20, true);
+// Nhưng mà bây giờ giả dụ chúng ta quên mất thứ tự của parameter thì làm sao?
+// Vậy để phòng trường hợp ta có thể quên mất thì ta phải viết function như sau:
+
+// Nếu ta làm thế này thì sẽ dễ dàng lấy ra được giá trị của object
+// function whatYourInfo2(obj) {
+//   console.log(obj.name, obj.age, obj.isMale);
+// }
+// const newStudent = {
+//   name: "Nam",
+//   age: 29,
+//   isMale: true,
+// };
+
+// whatYourInfo2(newStudent);
+// Vậy nên ta sẽ sử dung Object Destructuring Parameter nếu không nhớ thứ tự của tham số truyền vào
+
+function whatYourInfo2({ name, age, isMale }) {
+  console.log(name, age, isMale);
+}
+
+whatYourInfo2({
+  age: 29,
+  name: "Nam",
+  isMale: true,
+});
+// Bất kể tham số truyền bên trong viết theo thứ tự nào thì ta vẫn
+// có thể truyền vào bình thường mà không cần nhớ thứ tự
