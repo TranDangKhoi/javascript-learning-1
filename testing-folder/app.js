3b512477a31b5b891944c56578cbe324067c6598
TranDangKhoi <baits16@gmail.com>
2023-05-15 17:40:22 +0700

    🤔 Maybe this's the solution

--- testing-folder/app.js
+++ testing-folder/app.js
@@ -1,4 +1,9 @@
-const weekDays = [
+// const obj1 = {};s
+
+// console.log(obj2.hasOwnProperty("a"));
+// console.log(obj1.hasOwnProperty("a"));
+
+const daysInAWeek = [
   "Thứ 2",
   "Thứ 3",
   "Thứ 4",
@@ -8,10 +13,10 @@ const weekDays = [
   "Chủ nhật",
 ];
 
-function whateverItIs(arr, x) {
-  arr.splice(x, 6);
-  arr.splice(6 - 1, x);
-  console.log(arr);
-}
-
-whateverItIs(weekDays, 0);
+const move = (x) => {
+  let arr = daysInAWeek.splice(x, daysInAWeek.length - 1);
+  arr.unshift(daysInAWeek[daysInAWeek.length - 1]);
+  return arr;
+};
+console.log(move(0));
+console.log(move(1));
