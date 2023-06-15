3ed6c7c6960fafbb62086600fb013c8d95c93b05
TranDangKhoi <baits16@gmail.com>
2023-06-15 21:40:06 +0700

    :+1: Update algs

--- testing-folder/app.js
+++ testing-folder/app.js
@@ -1,20 +1,43 @@
-const daysInAWeek = [
-  "Thứ 2",
-  "Thứ 3",
-  "Thứ 4",
-  "Thứ 5",
-  "Thứ 6",
-  "Thứ 7",
-  "Chủ nhật",
-];
-// x = 0 =>  [CN, T2, T3, ...]
-// x = 1 =>  [T2, T3 ..., CN]
-// x = 2 => [T3, T4, ... CN, T2]
+// var sortArray = function (nums) {
+//   for (let i = 0; i < nums.length; ++i) {
+//     var min_index = i;
+//     for (let j = i + 1; j < nums.length; ++j) {
+//       if (nums[j] < nums[min_index]) {
+//         min_index = j;
+//       }
+//     }
+//     if (min_index !== i) {
+//       var temp = nums[i];
+//       nums[i] = nums[min_index];
+//       nums[min_index] = temp;
+//     }
+//     console.log(nums);
+//   }
+// };
 
-const move = (x) => {
-  let arr = daysInAWeek.splice(x, daysInAWeek.length - 1);
-  arr.unshift(daysInAWeek[daysInAWeek.length - 1]);
-  return arr;
-};
+// console.log(sortArray([9, 1, 2, 4, 10, 3, -10]));
 
-console.log(move(1));
+// const removeDuplicated = (nums) => {
+//   const removedDuplicatedArray = nums.filter((num, index) => {
+//     return nums.indexOf(num) === index;
+//   });
+//   return removedDuplicatedArray;
+// };
+
+// const whateverSortThisIs = (nums) => {
+//   for (let i = 0; i < nums.length; ++i) {
+//     // var minIndex = 0;
+//     // chạy liên tiếp bao giờ xong thì mới quay lại vòng lặp lồng nó
+//     for (let j = i + 1; j < nums.length; ++j) {
+//       if (nums[j] < nums[i]) {
+//         var temp = nums[i]; // temp = 9
+//         nums[i] = nums[j]; // num[i] = 11
+//         nums[j] = temp; // num[j] = 9 = temp
+//         console.log(nums);
+//       }
+//     }
+//   }
+//   return nums;
+// };
+
+// console.log(whateverSortThisIs([9, 1, 2, 4, 10, 3, -10]));
